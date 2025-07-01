147c0af4c34cff8b205eef9f81ddc9d4b1fbf69c
Soneyin <Soneyin@atguigu.com>
2025-07-01 13:52:08 +0800

    first commit cppBeginning

---
+++ cppBeginning/基本框架/基本框架及注释.cpp
@@ -0,0 +1,6 @@
+#include<iostream>  //使用头文件 包含cout cin
+using namespace std; //使用名字空间
+int main() {    //主程序 每个程序都必须写main()
+	cout << "hello world" << endl;
+	return 0;   //每个main()都要有return 0表示程序正常执行完毕
+}
